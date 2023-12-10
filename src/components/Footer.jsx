302a88db8b11786ad9e6f269e27b581486797dd3
tmayankv <106771483+tmayankv@users.noreply.github.com>
2023-12-10 13:18:21 +0530

    Improved some events

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-white text-gray-700 text-center m-22">
-      © 2023 All rights reserved by Admind.com
+      © 2023 All rights reserved by AdminLookout.com
     </p>
   </div>
 );
